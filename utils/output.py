e9f30cfa299440d0361483033a8e135fd6aae5fe
jgchoti <jgchotirat@gmail.com>
2025-06-18 13:20:13 +0200

    Refactor save_to_csv method to accept filename as a parameter

--- utils/output.py
+++ utils/output.py
@@ -6,31 +6,15 @@ from utils.config import ALL_KEYS
 
 class Output:
     def __init__(self):
-        self.base_name = "properties"
         self.columns = ["zimmo code"] + ALL_KEYS
-        
-    def get_filename(self):
-        path = os.path.abspath("")
-        data_path = os.path.join(path, "data")
-        os.makedirs(data_path, exist_ok=True)
-        index = 1
-        while True:
-            access_file = os.path.join(data_path, f"{self.base_name}{index}.csv")
-            if not os.path.exists(access_file):
-                return access_file
-            index += 1
 
-    def save_to_csv(self, data, overwrite=False):
+    def save_to_csv(self, filename, data, overwrite=False):
         df = pd.DataFrame.from_dict(data, orient="index")
-        
         path = os.path.abspath("")
-        access_file = self.get_filename()
-        
+        data_folder = os.path.join(path, "data")
+        access_file = os.path.join(data_folder, filename)
+    
         os.makedirs(os.path.dirname(access_file), exist_ok=True)
-
-        # df["year built"] = pd.to_numeric(df["year built"], errors="coerce").astype(
-        #     "Int64"
-        # )
         for col in self.columns[1:]:  
             if col not in df.columns:
                 df[col] = pd.NA
