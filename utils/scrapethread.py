79e3966de441b4ba2affbba70b7edae242d9e03f
jgchoti <jgchotirat@gmail.com>
2025-06-18 12:12:53 +0200

    Refactor ScrapeThread

--- utils/scrapethread.py
+++ utils/scrapethread.py
@@ -1,18 +1,18 @@
 import threading
 from utils.scraper import Scraper
-
+import pandas as pd
 class ScrapeThread(threading.Thread):
-    def __init__(self, url, results, id_counter, lock):
+    def __init__(self, url, results, lock):
         super().__init__()
         self.url = url
         self.results = results
-        self.id_counter = id_counter
         self.lock = lock
 
     def run(self):
-        local_scraper = Scraper()
-        with self.lock:
-            id_property = next(self.id_counter)
-        data = local_scraper.scrape_property(self.url, id_property)
-        with self.lock:
-            self.results[id_property] = data
+        thread_scraper = Scraper()
+        results = thread_scraper.scrape_property(self.url)
+        if results is not None:
+            zimmo_code, data = results
+            if zimmo_code is not None and data is not None:
+                with self.lock:
+                    self.results[zimmo_code] = data
